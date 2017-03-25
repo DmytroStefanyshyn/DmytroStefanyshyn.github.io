caf52fd5abd2363fdc2282fd7d6d8a1fa550bbbe
Dmytro <hellohowareyouuuu@gmail.com>
2017-03-25 22:51:26 +0200

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,4 +1,5 @@
 function task1(parent) {
+	main.innerText = "";
 	var keys = [];
 	for (var j = 1; j <= 9; j++) {
 		for (var i = 1;i <= 9;i++) { 
@@ -9,6 +10,7 @@ function task1(parent) {
 	var table = document.createElement("table");
 	table.setAttribute("cellpadding", 10);
 	table.setAttribute("align", "center");
+	main.style.textDecoration="none";
 	for (var i = 0; i < 9;i++) {
 		var tr = document.createElement("tr");
 		for (var j = 0;j < 9;j++) {
@@ -19,6 +21,37 @@ function task1(parent) {
 					td.setAttribute("bgcolor", "yellow");
 				} else {
 					td.setAttribute("bgcolor", "white");
+				}
+			counter++;
+		}
+		table.appendChild(tr);
+	}
+	parent.appendChild(table);
+}
+
+function task2(parent) {
+	main.innerText = "";
+	var keys = [];
+	for (var j = 1; j <= 9; j++) {
+		for (var i = 1;i <= 9;i++) { 
+			keys.push((j*i).toString());
+		}
+	}
+	var counter = 0;
+	var table = document.createElement("table");
+	table.setAttribute("cellpadding", 10);
+	table.setAttribute("align", "center");
+	main.style.textDecoration="underline";
+	for (var i = 0; i < 9;i++) {
+		var tr = document.createElement("tr");
+		for (var j = 0;j < 9;j++) {
+			var td = document.createElement("td");
+			td.innerText = keys[counter];
+			tr.appendChild(td);
+				if (keys[counter] % 2 == 0){
+					td.setAttribute("bgcolor", "white");
+				} else {
+					td.setAttribute("bgcolor", "yellow");
 				}
 			counter++;
 		}
